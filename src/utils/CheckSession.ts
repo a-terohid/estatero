0758bc6fadac280f47e0e069a4c42c18cc207cc5
a-terohid <87315139+a-terohid@users.noreply.github.com>
2025-04-26 16:37:33 -0400

    add checkSession function

---
+++ src/utils/CheckSession.ts
@@ -0,0 +1,31 @@
+import { Agent_Interface, User_Interface } from "@/types/modelTypes";
+import connectDB from "./connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import User from "@/models/user";
+import Agent from "@/models/agent";
+
+export const checkSession = async (): Promise<{ session: any; user: User_Interface | Agent_Interface | null }> => {
+  await connectDB();
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user || !session.user.role) {
+    throw new Error("Unauthorized: No valid session found");
+  }
+
+  const role = session.user.role;
+  let user: User_Interface | Agent_Interface | null = null;
+
+  if (role.includes("agent")) {
+    user = await Agent.findOne({ email: session.user.email });
+  } else {
+    user = await User.findOne({ email: session.user.email });
+  }
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return { session, user };
+};
