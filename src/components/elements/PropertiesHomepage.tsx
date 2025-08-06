c2c8db8746aad06c520aa09e53d029d6abc1d0e4
a-terohid <87315139+a-terohid@users.noreply.github.com>
2025-08-06 05:04:55 -0400

    fixed homepage propreties section

--- src/components/elements/PropertiesHomepage.tsx
+++ src/components/elements/PropertiesHomepage.tsx
@@ -34,7 +34,7 @@ const PropertiesHomepage = () => {
         <div className=" container">
             <h1 className="text-Heading-4 md:text-Heading-2 lg:text-Heading-1 text-center md:px-16 lg:px-60">Turning Your Real Estate Dreams Into Reality</h1>
             <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {loading
+                {loading && !Properties.length
                 ? [...Array(6)].map((_, i) => (
                     <div key={i} className="">
                         <PropertyCardSkeleton />
